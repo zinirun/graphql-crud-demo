963f8abfa38da12161837a0033756e47a7da747b
zinirun <heojj97@naver.com>
2020-10-26 15:09:51 +0900

    Add CRUD Example

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
-const {graphqlHTTP} = require('express-graphql');
-const {buildSchema} = require('graphql');
+const { graphqlHTTP } = require('express-graphql');
+const { buildSchema } = require('graphql');
 const cors = require('cors');
 const port = 3000;
 
@@ -19,12 +19,14 @@ const schema = buildSchema(`
     }
 
     type Query {
-        getProduct(id: ID!): Product 
+        getProduct(id: ID!): Product
+        getProducts: [Product]
+        searchProducts(name: String): [Product]
     }
 
     type Mutation {
         addProduct(input: ProductInput): Product
-        updateProduct(id: ID!, input: ProductInput!): Product
+        updateProduct(id: ID!, input: ProductInput!): String
         deleteProduct(id: ID!): String
     }
 `);
@@ -32,52 +34,63 @@ const schema = buildSchema(`
 const products = [
     {
         id: 1,
-        name: '첫번째 제품',
+        name: 'First Product',
         price: 2000,
-        description: '하하하',
+        description: 'This is first product',
     },
     {
         id: 2,
-        name: '두번째 제품',
+        name: 'Second Product',
         price: 4000,
-        description: '호호호',
-    }
-]
+        description: 'This is second product',
+    },
+];
 
 const rootValue = {
-    getProduct: ({id}) => products.find(product => product.id === parseInt(id)),
-
-    addProduct: ({input}) => {
-        input.id = products.length+1;
-        products.push(input);
-        return rootValue.getProduct({id: input.id});
+    getProduct: ({ id }) => {
+        return products.find((product) => product.id === parseInt(id));
     },
 
-    updateProduct: ({id, input}) => {
-        const index = products.findIndex(product => product.id === parseInt(id));
+    getProducts: () => products,
+
+    searchProducts: ({ name }) => {
+        return products.filter((product) => product.name.toLowerCase().includes(name));
+    },
+
+    addProduct: ({ input }) => {
+        input.id = products.length === 0 ? 1 : products[products.length - 1].id + 1;
+        products.push(input);
+        return rootValue.getProduct({ id: input.id });
+    },
+
+    updateProduct: ({ id, input }) => {
+        const index = products.findIndex((product) => product.id === parseInt(id));
         products[index] = {
             id: parseInt(id),
-            ...input
-        }
-        return products[index];
+            ...input,
+        };
+        return id;
     },
 
-    deleteProduct: ({id}) => {
-        const index = products.findIndex(product => product.id === parseInt(id));
+    deleteProduct: ({ id }) => {
+        const index = products.findIndex((product) => product.id === parseInt(id));
         products.splice(index, 1);
-        return "Remove Success";
-    }
+        return id;
+    },
 };
 
 const app = express();
 app.use(cors()); // to use Postman
-app.use('/static', express.static('static'));
-app.use('/graphql', graphqlHTTP({
-    schema,
-    rootValue,
-    graphiql: true, // Support GUI (false when publishing)
-}));
+app.use('/', express.static('static'));
+app.use(
+    '/graphql',
+    graphqlHTTP({
+        schema,
+        rootValue,
+        graphiql: true, // Support GUI (false when publishing)
+    }),
+);
 
-app.listen(port, ()=>{
+app.listen(port, () => {
     console.log(`running server port ${port}`);
 });
